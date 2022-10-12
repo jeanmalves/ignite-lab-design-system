489639c82ae70d771edb28ed1f141aab24d25f31
Jeam Alves <jeanmalves@gmail.com>
2022-10-12 17:52:54 -0400

    Text component doc with variations

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,22 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Text, TextProps } from "./Text";
+
+export default {
+  title: 'Components/Text',
+  component: Text,
+  args: { children: 'Lorem ipsum.' }
+} as Meta<TextProps>
+
+export const Default: StoryObj<TextProps> = {}
+
+export const Small: StoryObj<TextProps> = {
+  args: {
+    size: 'sm'
+  }
+}
+
+export const Large: StoryObj<TextProps> = {
+  args: {
+    size: 'lg'
+  }
+}
